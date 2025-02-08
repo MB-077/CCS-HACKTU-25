bc0817cdad1757151ac2cbd66ca616c9d0ca911b
ishwin07 <syal.ishwin@gmail.com>
2025-02-08 21:14:26 +0530

    Fresh start for streamlit

--- Agri_Space/Ml_model/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('crop_prediction/', views.Crop_Prediction.as_view(), name='crop_prediction'),
-    path('fertilizer_prediction/', views.Fertilizer_Prediction.as_view(), name='fertilizer_prediction'),
-]
