709f30f4a89f41235f79e414f0ee3eaddf7c08f0
Marul Bhardwaj <158758744+MB-077@users.noreply.github.com>
2025-02-08 19:05:21 +0530

    sensor backend integration done

--- Agri_Space/Sensors/admin.py
+++ Agri_Space/Sensors/admin.py
@@ -57,3 +57,72 @@ class SensorDataAdmin(ModelAdmin):
     )
 
 admin.site.register(SensorData, SensorDataAdmin)
+
+
+class ChildNodeSensorDataAdmin(ModelAdmin):
+    list_display = ('esp_device_id', 'child_node_id', 'timestamp', 'node_dht11_humidity', 'node_dht11_temperature', 'node_soil_moisture_percent_2', 'node_DS18B20_temperature', 'node_rain_percent',)
+    list_filter = ('esp_device_id', 'child_node_id', 'timestamp',)
+    search_fields = ('esp_device_id', 'child_node_id', 'timestamp',)
+    ordering = ('-timestamp',)
+    readonly_fields = ('esp_device_id', 'child_node_id', 'timestamp', 'node_dht11_humidity', 'node_dht11_temperature', 'node_soil_moisture_raw_2', 'node_soil_moisture_percent_2', 'node_DS18B20_temperature', 'node_rain_raw', 'node_rain_percent',)
+    list_display_links = ('timestamp', 'esp_device_id', 'child_node_id',)
+    list_per_page = 20
+    list_max_show_all = 100
+    
+    fieldsets = (
+        ('Node DHT11 Humidity & Temperature', {
+            'fields': ('node_dht11_humidity', 'node_dht11_temperature',)
+        }),
+        ('Node Soil Moisture 2', {
+            'fields': ('node_soil_moisture_raw_2', 'node_soil_moisture_percent_2',)
+        }),
+        ('Node DS18B20 Temperature', {
+            'fields': ('node_DS18B20_temperature',)
+        }),
+        ('Node Rain Sensor', {
+            'fields': ('node_rain_raw', 'node_rain_percent',)
+        }),
+        (None, {
+            'fields': ('esp_device_id', 'child_node_id', 'timestamp', )
+        }),
+    )
+
+admin.site.register(ChildNodeSensorData, ChildNodeSensorDataAdmin)
+
+
+class IrrigationCycleInline(TabularInline):
+    model = IrrigationCycle
+    extra = 0
+    
+class LastIrrigationCycleInline(TabularInline):
+    model = LastIrrigationCycle
+    extra = 0
+    readonly_fields = ('relay_state', 'irrigation_cycle', 'timestamp', )
+    tab = True
+    
+
+class RelayStateAdmin(ModelAdmin):
+    inlines = [IrrigationCycleInline, LastIrrigationCycleInline]
+    list_display = ('day_of_week', 'state', 'last_updated')
+    list_filter = ('day_of_week',)
+    search_fields = ('day_of_week',)
+    ordering = ('day_of_week',)
+    readonly_fields = ('last_updated',)
+
+admin.site.register(RelayState, RelayStateAdmin)   
+
+
+class WaterConsumptionAdmin(ModelAdmin):
+    list_display = ('flow_rate', 'irrigation_cycle', 'individual_usage', 'total_measurement_duration_sec',)
+    list_filter = ('irrigation_cycle',)
+    search_fields = ('flow_rate', 'irrigation_cycle',)
+    readonly_fields = ('flow_rate', 'irrigation_cycle', 'individual_usage', 'total_measurement_duration_sec',)
+    
+admin.site.register(WaterConsumption, WaterConsumptionAdmin)
+
+
+class TotalWaterUsageAdmin(ModelAdmin):
+    list_display = ('total_usage',)
+    readonly_fields = ('total_usage',)
+    
+admin.site.register(TotalWaterUsage, TotalWaterUsageAdmin)
---
+++ Agri_Space/mongo_data/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MongoDataConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mongo_data'
