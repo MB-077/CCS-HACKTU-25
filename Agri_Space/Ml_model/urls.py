e3881cd6335216c8d4a77e95debe4a8d0acd280e
Ritwik Gupta <ritwikg.0501@gmail.com>
2025-02-08 18:50:24 +0530

    Added LFS tracking

--- Agri_Space/Ml_model/urls.py
+++ Agri_Space/Ml_model/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('crop_prediction/', views.Crop_Prediction.as_view(), name='crop_prediction'),
     path('fertilizer_prediction/', views.Fertilizer_Prediction.as_view(), name='fertilizer_prediction'),
+    path('yield/', views.Yield_Prediction.as_view(), name='predict'),
 ]
